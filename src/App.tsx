e9c2f261d670ba77bfdd606cf373cdc180403fa6
unknown <miguel317.angelps@gmail.com>
2026-02-04 13:39:08 +0100

    Configuracion inicial TSX y definicion de interfaz Pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+//He definido la interfaz para los objetos Pokemon
+// Ya que la API devuelve varios datos, pero solo usaremos estos tres asegurando que el ID es un número
+interface Pokemon { 
+  ID: number;
+  nombre: string;
+  url: string;
+ 
+}
+
 function App() {
   const [pokemon, setPokemon] = useState([])
 
@@ -8,18 +17,19 @@ function App() {
 
   },[])
 
-  async function getPokemon(page){
+  /*async function getPokemon(page){
 
     fetch(`https://pokeapi.co/api/v2/pokemon?limit=${}&offset=${}`).then(res =>
       res.json()
     ).then(data => {
       console.log(data)
     })
-  }
+  }*/
 
   return (
     <>
       {
+      <h1>Cargando Pokemons...</h1>
         /*
           Lista de pokemon
         */
