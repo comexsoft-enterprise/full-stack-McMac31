50d8f6f6eb6c41a65e85409c041ff81c31140cf1
unknown <miguel317.angelps@gmail.com>
2026-02-04 14:02:04 +0100

    Obtencion de datos de datos de la API

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,47 @@ import './App.css'
 //He definido la interfaz para los objetos Pokemon
 // Ya que la API devuelve varios datos, pero solo usaremos estos tres asegurando que el ID es un número
 interface Pokemon { 
-  ID: number;
-  nombre: string;
+  id: number;
+  name: string;
   url: string;
  
 }
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
+  //Estados para manejar los pokemons, la librería y el pokedex
+  const [library, setLibrary] = useState<Pokemon[]>([])
+  const [pokedex, setPokedex] = useState<Pokemon[]>([])
+  const[selectedId, setSelectedId]= useState<number | null>(null)
 
+// useEffect para cargar los pokemons al montar el componente
   useEffect(()=>{
-
-  },[])
-
-  /*async function getPokemon(page){
-
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${}&offset=${}`).then(res =>
-      res.json()
-    ).then(data => {
-      console.log(data)
-    })
-  }*/
-
+    const fetchPokemons = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+        const data = await response.json();
+        const cleanPokemons: Pokemon[] = data.results.map((p: any) => {
+        const parts = p.url.split('/'); // Divide la URL para extraer el ID
+        const id = parseInt(parts[parts.length - 2]); // Extrae el ID de la URL
+        return {
+          name: p.name,
+          url: p.url,
+          id: id
+        };
+      });
+        setLibrary(cleanPokemons);
+      } catch (error) {
+        console.error('Error cargando pokemons:', error);
+      }
+    };
+    fetchPokemons();
+  }, []);
+  //Prueba de renderizado
   return (
     <>
       {
-      <h1>Cargando Pokemons...</h1>
-        /*
-          Lista de pokemon
-        */
+      <div>
+      <h1>Cargando {library.length} Pokemons... </h1>
+      </div>
       }
     </>
   )
